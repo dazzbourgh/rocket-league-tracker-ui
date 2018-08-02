a07ca2ac3db0752f90f3c275f53dec73151ac474
PETERSBURG\Leonid_Borisevich <leonid_borisevich@epam.com>
2018-08-02 20:06:15 +0300

    visual enhancements

--- src/app/offer-item/offer-item.component.ts
+++ src/app/offer-item/offer-item.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'rl-offer-item',
-  templateUrl: './offer-item.component.html'
+  templateUrl: './offer-item.component.html',
+  styleUrls: ['./offer-item.component.css']
 })
 export class OfferItemComponent implements OnInit {
 
